9c0403a68c015113dcf2feb4b7a225b4d9af2a7d
Victor Frolov <vicfrolov@gmail.com>
2016-09-09 18:25:05 -0700

    initial commit for iOS app

---
+++ iOS/Markit/Markit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Markit
+//
+//  Created by Victor Frolov on 9/9/16.
+//  Copyright © 2016 Victor Frolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
