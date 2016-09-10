ac945226599fd86bdea803e9b52e696280936757
Victor Frolov <vicfrolov@gmail.com>
2016-09-09 21:01:17 -0700

    added view controllers and tab bar, setting launch index of tabbar to 1 programmatically in delegate

--- iOS/Markit/Markit/AppDelegate.swift
+++ iOS/Markit/Markit/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //setting the tabbar to make the launch index 1
+        let tabBar: UITabBarController = self.window?.rootViewController as! UITabBarController;
+        tabBar.selectedIndex = 1;
+        
         return true
     }
 
