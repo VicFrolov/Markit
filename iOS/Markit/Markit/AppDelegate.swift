5a6aa01264b43ea742c698e335c3691b93751d6a
Victor Frolov <vicfrolov@gmail.com>
2016-09-11 18:46:53 -0700

    added two new tabbar buttons, and playing with color for highlighted tab in appDelegate

--- iOS/Markit/Markit/AppDelegate.swift
+++ iOS/Markit/Markit/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        
+        UITabBar.appearance().tintColor = UIColor(red: 30/255.0, green: 144/255.0, blue: 255/255.0, alpha: 1.0)
+
         //setting the tabbar to make the launch index 1
         let tabBar: UITabBarController = self.window?.rootViewController as! UITabBarController;
         tabBar.selectedIndex = 1;
