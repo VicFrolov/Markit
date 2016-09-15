0661ab038ebcb073c49b5e027706880c4c772689
Victor Frolov <vicfrolov@gmail.com>
2016-09-15 15:48:30 -0700

    added template for website, currently navbar with no search section and drop down

---
+++ mockup-webapp/js/custom.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(function() {
+
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            hover: true, // Activate on hover
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'right' // Displays dropdown with edge aligned to the left of button
+        });
+
+    }); // End Document Ready
+})(jQuery); // End of jQuery name space
