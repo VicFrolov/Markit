0f15666d8df19d736c5159a5ddefd2666aa33c60
Victor Frolov <vicfrolov@gmail.com>
2016-09-17 19:28:14 -0700

    added a pop up on click login and close pop up if clicked outside of popup

--- mockup-webapp/js/custom.js
+++ mockup-webapp/js/custom.js
@@ -1,6 +1,19 @@
 $(function() {
+    $('#login-popup').hide();
 
     $('.carousel.carousel-slider').carousel({full_width: true});
+
+    $('#login-button').on('click', function () {
+        $('#login-popup').fadeIn();
+    });
+
+    $(document).mouseup(function (e) {
+        var popup = $('#login-popup');
+        console.log(!popup.is(e.target));
+        if (popup.is(e.target)) {
+            popup.fadeOut();
+        }
+    });    
 
         // $('.dropdown-button').dropdown({
         //     inDuration: 300,
@@ -10,35 +23,35 @@ $(function() {
         //     alignment: 'right' // Displays dropdown with edge aligned to the left of button
         // });
 
-    // $("nav").mouseenter(function(){
+    // $('nav').mouseenter(function(){
     //     $(this).animate({
-    //         "margin-left": "0%",
-    //         width: "100%"
+    //         'margin-left': '0%',
+    //         width: '100%'
     //     });
         
-    //     $(".brand-logo").animate({
-    //         "margin-left": "40px"
+    //     $('.brand-logo').animate({
+    //         'margin-left': '40px'
     //     });
 
-    //     $("nav ul li").animate({
-    //         "padding-right": "10px",
-    //         "padding-left": "10px"
+    //     $('nav ul li').animate({
+    //         'padding-right': '10px',
+    //         'padding-left': '10px'
     //     });
     // });
 
-    // $("nav").mouseleave(function(){
+    // $('nav').mouseleave(function(){
     //     $(this).animate({
-    //         "margin-left": "7%",
-    //         width: "84%"
+    //         'margin-left': '7%',
+    //         width: '84%'
     //     });
 
-    //     $(".brand-logo").animate({
-    //         "margin-left": "0px"
+    //     $('.brand-logo').animate({
+    //         'margin-left': '0px'
     //     });
        
-    //     $("nav ul li").animate({
-    //         "padding-right": "0px",
-    //         "padding-left": "0px"
+    //     $('nav ul li').animate({
+    //         'padding-right': '0px',
+    //         'padding-left': '0px'
     //     })
     // });    
 
