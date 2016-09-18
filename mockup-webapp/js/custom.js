97f548504966db9f811a06081c7e00d3f7d3b6df
Victor Frolov <vicfrolov@gmail.com>
2016-09-17 19:46:54 -0700

    added an or line filter as well as facebook/google buttons with margins and padding

--- mockup-webapp/js/custom.js
+++ mockup-webapp/js/custom.js
@@ -15,44 +15,4 @@ $(function() {
         }
     });    
 
-        // $('.dropdown-button').dropdown({
-        //     inDuration: 300,
-        //     outDuration: 225,
-        //     hover: true, // Activate on hover
-        //     belowOrigin: true, // Displays dropdown below the button
-        //     alignment: 'right' // Displays dropdown with edge aligned to the left of button
-        // });
-
-    // $('nav').mouseenter(function(){
-    //     $(this).animate({
-    //         'margin-left': '0%',
-    //         width: '100%'
-    //     });
-        
-    //     $('.brand-logo').animate({
-    //         'margin-left': '40px'
-    //     });
-
-    //     $('nav ul li').animate({
-    //         'padding-right': '10px',
-    //         'padding-left': '10px'
-    //     });
-    // });
-
-    // $('nav').mouseleave(function(){
-    //     $(this).animate({
-    //         'margin-left': '7%',
-    //         width: '84%'
-    //     });
-
-    //     $('.brand-logo').animate({
-    //         'margin-left': '0px'
-    //     });
-       
-    //     $('nav ul li').animate({
-    //         'padding-right': '0px',
-    //         'padding-left': '0px'
-    //     })
-    // });    
-
-}); // End Document Ready
+});
