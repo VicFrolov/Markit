5114f4f6d2973a47e472c84bc50621c944be5195
Chief Keef <pcross2@lion.lmu.edu>
2016-09-27 20:41:40 -0700

    Renamed the applicationId

--- android/Markit/app/src/main/java/com/markit/markit/MainActivity.java
+++ android/Markit/app/src/main/java/com/markit/markit/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,8 +22,10 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     private EditText emailView;
     private EditText passwordView;
     private EditText confirmPasswordView;
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
         emailView = (EditText) findViewById(R.id.email);
         passwordView = (EditText) findViewById(R.id.password);
         confirmPasswordView = (EditText) findViewById(R.id.password2);
@@ -42,18 +49,65 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 attemptRegister();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
     }
 
     private void attemptRegister() {
+        Firebase userRef = new  Firebase("http://markit-80192.firebaseio.com");
         String email = emailView.getText().toString();
         String password = passwordView.getText().toString();
         String confirmPassword = confirmPasswordView.getText().toString();
+        boolean cancel = false;
 
-        //Checking email
+        if (!isEmailValid(email)) {
+            emailView.setError("Please enter in a valid .edu email");
+            cancel = true;
+        }
+
+        if(!isPasswordValid(password)) {
+            passwordView.setError("Your password sucks enter a better one");
+            cancel = true;
+        }
+
+        if(!passwordMatch(password, confirmPassword)) {
+            passwordView.setError("Your passwords do not match");
+            cancel = true;
+        }
+
+        if (cancel) {
+            //If there was an error. Probably going to focusview on the problem.
+            emailView.requestFocus();
+        } else {
+          //Attempt to register account
+
+            //Toast.makeText(MainActivity.this, "Got to the else condition", Toast.LENGTH_LONG).show();
+            userRef.createUser(email,password, new Firebase.ValueResultHandler<Map<String, Object>>(){
+                @Override
+                public void onSuccess(Map<String, Object> result) {
+                    Toast.makeText(MainActivity.this, "Account successfully registered", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    Toast.makeText(MainActivity.this, firebaseError.toString(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+
+        }
+
     }
 
+    private boolean isEmailValid(String email) {
+        return email.contains("@") && email.contains(".edu");
+    }
 
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+    private boolean passwordMatch(String password, String confirmedPassword) {
+        return password.equals(confirmedPassword);
+    }
 }
