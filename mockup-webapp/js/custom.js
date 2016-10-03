ff92c83585ee19afaf2ebdb54abafefeb97d2823
Victor Frolov <vicfrolov@gmail.com>
2016-10-03 00:27:11 -0700

    stylistic changes to noUiSlider

--- mockup-webapp/js/custom.js
+++ mockup-webapp/js/custom.js
@@ -6,12 +6,14 @@ $(function() {
     noUiSlider.create(slider, {
         start: [0, 300],
         connect: true,
+        step: 1,
         tooltips: true,
         range: {
             'min': 0,
             'max': 1500
         }
     });
+
 
     /**
         loading main data, slight timeOut so it 
