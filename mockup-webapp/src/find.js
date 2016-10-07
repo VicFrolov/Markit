ff489ab7c2d75e752cf456f0ad9756658941276f
Victor Frolov <vicfrolov@gmail.com>
2016-10-06 17:14:13 -0700

    added favorite icon, and changed z-index of navbar pop up login to always be on top

--- mockup-webapp/src/find.js
+++ mockup-webapp/src/find.js
@@ -26,45 +26,56 @@ $(function() {
                 "./iphone-sample.jpg"
             imageSwitcher = !imageSwitcher;
 
-            $("#find-content").append($("<div></div>").addClass("col l4 m4 s12").append(
-                $("<div></div>").addClass("card find-result").append(
-                    $("<div></div>").addClass("card-image waves-effect waves-block waves-light").append(
-                        $("<img/>").addClass("activator").attr({
-                            src: currentImage
-                        })
-                    )
-                ).append(
-                    $("<div></div>").addClass("card-content").append(
-                        $("<span></span>").addClass("card-title activator grey-text text-darken-4").text(
-                                currentItem["item"]
-                        ).append(
-                            $("<i></i>").addClass("material-icons right").text("more_vert")
+            $("#find-content").append(
+                $("<div></div>").addClass("col l4 m4 s12").append(
+                    $("<div></div>").addClass("card find-result").append(
+                        $("<div></div>").addClass("find-result-favorite").append(
+                            $("<img/>").addClass("find-result-favorite-image").attr({
+                                src: "../media/ic_heart.png",
+                                alt: "heart"
+                            })
                         )
                     ).append(
-                        $("<p></p>").append(
-                            $("<a></a>").attr({
-                                href: "#"
-                            }).text(
-                                "view item"
+                        $("<div></div>").addClass("find-result-price").text(
+                            "$" + currentItem["price"])).append(
+                        $("<div></div>").addClass("card-image waves-effect waves-block waves-light").append(
+                            $("<img/>").addClass("activator").attr({
+                                src: currentImage
+                            })
+                        )
+                    ).append(
+                        $("<div></div>").addClass("card-content").append(
+                            $("<span></span>").addClass("card-title activator grey-text text-darken-4").text(
+                                    currentItem["item"]
+                            ).append(
+                                $("<i></i>").addClass("material-icons right").text("more_vert")
+                            )
+                        ).append(
+                            $("<p></p>").append(
+                                $("<a></a>").attr({
+                                    href: "#"
+                                }).text(
+                                    "view item"
+                                )
                             )
                         )
-                    )
-                ).append(
-                    $("<div></div>").addClass("card-reveal").append(
-                        $("<span></span>").addClass("card-title grey-text text-darken-4").text(
-                            "Description"
-                        ).append(
-                            $("<i></i>").addClass("material-icons right").text(
-                                "close"
-                            )
-                        ).append(
-                            $("<p></p>").text(
-                                currentItem["description"]
+                    ).append(
+                        $("<div></div>").addClass("card-reveal").append(
+                            $("<span></span>").addClass("card-title grey-text text-darken-4").text(
+                                "Description"
+                            ).append(
+                                $("<i></i>").addClass("material-icons right").text(
+                                    "close"
+                                )
+                            ).append(
+                                $("<p></p>").text(
+                                    currentItem["description"]
+                                )
                             )
                         )
                     )
                 )
-            ));
+            );
         };
     };
 
