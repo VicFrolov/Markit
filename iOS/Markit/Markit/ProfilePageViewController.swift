00f9b6b619607f5a069222c1d08a5dc5327f33e3
bku14 <bryan.ku@gmail.com>
2016-10-14 18:11:56 -0700

    Add customizable pagedots for profile

--- iOS/Markit/Markit/ProfilePageViewController.swift
+++ iOS/Markit/Markit/ProfilePageViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProfilePageViewController: UIPageViewController {
     
+    weak var profileDelegate: ProfilePageViewControllerDelegate?
+    
     private(set) lazy var orderedViewControllers: [UIViewController] = {
         return [self.newViewController(title: "ProfilePage"),
                 self.newViewController(title: "WatchListPage"),
@@ -24,7 +26,8 @@ class ProfilePageViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var dataSource = self
+        dataSource = self
+        delegate = self
         
         if let firstViewController = orderedViewControllers.first {
             setViewControllers([firstViewController],
@@ -32,6 +35,8 @@ class ProfilePageViewController: UIPageViewController {
                                animated: true,
                                completion: nil)
         }
+        
+        profileDelegate?.profilePageViewController(profilePageViewController: self, didUpdatePageCount: orderedViewControllers.count)
     }
     
     
@@ -39,13 +44,80 @@ class ProfilePageViewController: UIPageViewController {
 }
 
 extension ProfilePageViewController: UIPageViewControllerDataSource {
+    
     func pageViewController(_ pageViewController: UIPageViewController,
-                            viewControllerBefore viewController: UIViewController) ->UIViewController? {
-        return nil
-        }
-    func pageViewController(_ pageViewController: UIPageViewController,
-                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return nil
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
         }
         
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+        
+}
+
+extension ProfilePageViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            didFinishAnimating finished: Bool,
+                            previousViewControllers: [UIViewController],
+                            transitionCompleted completed: Bool) {
+        if let firstViewController = viewControllers?.first,
+            let index = orderedViewControllers.index(of: firstViewController) {
+                profileDelegate?.profilePageViewController(profilePageViewController: self, didUpdatePageIndex: index)
+        }
+    }
+    
+}
+
+protocol ProfilePageViewControllerDelegate: class {
+    
+    /**
+     Called when the number of pages is updated.
+     
+     - parameter tutorialPageViewController: the TutorialPageViewController instance
+     - parameter count: the total number of pages.
+     */
+    func profilePageViewController(profilePageViewController: ProfilePageViewController,
+                                    didUpdatePageCount count: Int)
+    
+    /**
+     Called when the current index is updated.
+     
+     - parameter tutorialPageViewController: the TutorialPageViewController instance
+     - parameter index: the index of the currently visible page.
+     */
+    func profilePageViewController(profilePageViewController: ProfilePageViewController,
+                                    didUpdatePageIndex index: Int)
+    
 }
