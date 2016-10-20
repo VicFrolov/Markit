736cc83e2c632f5139bdef2e654ff85debc7ac76
jb0001994 <jb0001994@aol.com>
2016-10-20 13:19:26 -0700

    Android - added fragment for the profile page

--- android/Markit/app/src/main/java/com/markit/android/ProfilePageFragment.java
+++ android/Markit/app/src/main/java/com/markit/android/ProfilePageFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.Markit.android.R;
+import com.markit.android.R;
 
 /**
  * A simple {@link Fragment} subclass.
