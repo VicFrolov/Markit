178a19bce03e47753f298635e622a0bda10904ba
jb0001994 <jb0001994@aol.com>
2016-10-22 18:41:58 -0700

    Android - added profile page to profile activity

--- android/Markit/app/src/main/java/com/markit/android/ProfilePageFragment.java
+++ android/Markit/app/src/main/java/com/markit/android/ProfilePageFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.graphics.drawable.Drawable;
 
 import com.markit.android.R;
 
