4a8f4d8e5324788e9fca2f60c0ed04a3910741fe
Victor Frolov <vicfrolov@gmail.com>
2016-11-06 16:09:20 -0800

    auto-filling tags working, added image dropzone

--- mockup-webapp/bin/index.bundle.js
+++ mockup-webapp/bin/index.bundle.js
@@ -790,7 +790,51 @@
 	                        result : textext.itemManager().filter(list, query) }
 	                    );
 	        });
-	    }   
+	    }
+
+
+	    // drophub
+	    var reader;
+	    var drop;
+	    
+	    $('#dropMain, .drop').on({
+	        'click': function(e) {
+	            $('#fileBox').click();
+	            drop = this;
+	            reader = new FileReader();
+	        },
+	        'dragover dragenter': function(e) {
+	            e.preventDefault();
+	            e.stopPropagation();
+	        },
+	        'drop': function(e) {
+	            var dropArea = this;
+	            var dataTransfer =  e.originalEvent.dataTransfer;
+	            if (dataTransfer && dataTransfer.files.length) {
+	                e.preventDefault();
+	                e.stopPropagation();
+	                $.each(dataTransfer.files, function(i, file) { 
+	                    reader = new FileReader();
+	                    reader.onload = $.proxy(function(file, $fileList, event) {
+	                        var img = file.type.match('image.*')
+	                            ? $("<img>").attr('src', event.target.result) : "";
+	                        $fileList.empty().append(img);
+	                    }, this, file, $(dropArea));
+	                    reader.readAsDataURL(file);
+	                });
+	            }
+	        }
+	    });
+
+
+	    $("#fileBox").change(function() {
+	        if (this.files && this.files[0]) {
+	            reader.onload = function (e) {
+	                $(drop).empty().append($("<img>").attr("src", reader.result));
+	            }
+	            reader.readAsDataURL(this.files[0]);
+	        }
+	    });
 
 	});
 
