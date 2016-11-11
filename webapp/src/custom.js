e0a6208fc47e47ffbd72fa00b753dc2b25c93324
Victor Frolov <vicfrolov@gmail.com>
2016-11-11 02:02:37 -0800

    adding tabs for right panel to logged in users in find page

--- webapp/src/custom.js
+++ webapp/src/custom.js
@@ -2,13 +2,4 @@ $(function() {
     // $('.slider').slider();
     $('ul.tabs').tabs();
 
-    /**
-        loading main data, slight timeOut so it 
-        doesn't appear before the navbar does
-    **/
-    // var loadMain = function () {
-    //     $('main').fadeIn()
-    // }
-    // setTimeout(loadMain, 300);
-
 });
