a75e80f6ac8eda0d3b535889514955ff0103bc01
Victor Frolov <vicfrolov@gmail.com>
2016-11-16 18:45:23 -0800

    adding responsive item description and user description divs

--- webapp/bin/index.bundle.js
+++ webapp/bin/index.bundle.js
@@ -56,23 +56,26 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var auth = __webpack_require__(2)["auth"];
+	$(function() {
+	    var auth = __webpack_require__(2)["auth"];
 
-	auth.onAuthStateChanged(function(user) {
-	    if (user) {
-	        console.log('user is signed in');
-	        $("#navbar-placeholder").load("../navbar/navbar-logged-in.html", function () {
-	            $(".dropdown-button").dropdown();
-	            $("#navbar-logout-button").click(function () {
-	                auth.signOut();
-	            })
-	        });
-	    } else {
-	        console.log('user is NOT signed in');
-	        $("#navbar-placeholder").load("../navbar/navbar-signup.html", function () {
-	            $(".dropdown-button").dropdown();
-	        });
-	    }
+	    auth.onAuthStateChanged(function(user) {
+	        if (user) {
+	            console.log('user is signed in');
+	            $("#navbar-placeholder").load("../navbar/navbar-logged-in.html", function () {
+	                $(".dropdown-button").dropdown();
+	                $("#navbar-logout-button").click(function () {
+	                    auth.signOut();
+	                })
+	            });
+	        } else {
+	            console.log('user is NOT signed in');
+	            $("#navbar-placeholder").load("../navbar/navbar-signup.html", function () {
+	                $(".dropdown-button").dropdown();
+	            });
+	        }
+	    });
+
 	});
 
 /***/ },
