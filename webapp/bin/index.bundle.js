391b266afd277c53f2c7cba46d7ae15704c3815e
vicfrolov <vicfrolov@gmail.com>
2016-11-27 21:09:37 -0800

    webapp- hub loading favorites into most recent to test lodash

--- webapp/bin/index.bundle.js
+++ webapp/bin/index.bundle.js
@@ -2099,11 +2099,44 @@
 
 /***/ },
 /* 14 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	$(function () {
 	    $('.parallax').parallax();
-	})
+	    var getFavoriteObjects = __webpack_require__(2)['getFavoriteObjects'];
+	    var itemImagesRef = __webpack_require__(2)["itemImagesRef"];
+
+
+	    var getImage = function(address, callback) {
+	        itemImagesRef.child(address).getDownloadURL().then(function(url) {
+	            callback(url);
+	        }).catch(function(error) {
+	            console.log("error image not found");
+	            console.log("error either in item id, filename, or file doesn't exist");
+	        });
+	    };
+
+	    var mostRecentItems = $('#hub-most-recent');
+	    var showFavoritesInSidebar = function(items) {
+	        var str = $('#hub-most-recent').text();
+	        var compiled = _.template(str);
+
+	        $('#hub-recent-holder').empty();
+	        $('#hub-recent-holder').append(compiled({items: items}));
+
+	        for (var i = 0; i < items.length; i += 1) {
+	            (function (x) {
+	                getImage(items[x]['id'] + '/imageOne', function(url) {
+	                    tagToAdd = ".card-image img:eq(" + x  + " )";
+	                    $(tagToAdd).attr({src: url});
+	                });
+	            })(i);
+	        }
+	    };
+
+	    getFavoriteObjects(showFavoritesInSidebar);
+
+	});
 
 /***/ }
 /******/ ]);
--- webapp/src/hub.js
+++ webapp/src/hub.js
@@ -1,3 +1,36 @@
 $(function () {
     $('.parallax').parallax();
-})
+    var getFavoriteObjects = require('./firebase.js')['getFavoriteObjects'];
+    var itemImagesRef = require('./firebase.js')["itemImagesRef"];
+
+
+    var getImage = function(address, callback) {
+        itemImagesRef.child(address).getDownloadURL().then(function(url) {
+            callback(url);
+        }).catch(function(error) {
+            console.log("error image not found");
+            console.log("error either in item id, filename, or file doesn't exist");
+        });
+    };
+
+    var mostRecentItems = $('#hub-most-recent');
+    var showFavoritesInSidebar = function(items) {
+        var str = $('#hub-most-recent').text();
+        var compiled = _.template(str);
+
+        $('#hub-recent-holder').empty();
+        $('#hub-recent-holder').append(compiled({items: items}));
+
+        for (var i = 0; i < items.length; i += 1) {
+            (function (x) {
+                getImage(items[x]['id'] + '/imageOne', function(url) {
+                    tagToAdd = ".card-image img:eq(" + x  + " )";
+                    $(tagToAdd).attr({src: url});
+                });
+            })(i);
+        }
+    };
+
+    getFavoriteObjects(showFavoritesInSidebar);
+
+});
