9ecb490a3fdb2a78b6487d3caa3b91d5efdbf0e9
Trixie Roque <troque@lion.lmu.edu>
2016-11-30 14:40:51 -0800

    Removed unused dependency, MLPAutoCompleteTextField

---
+++ iOS/Markit/Markit/AutoCompleteTextField.swift
@@ -0,0 +1,21 @@
+//
+//  AutoCompleteTextField.swift
+//  Markit
+//
+//  Created by Trixie on 11/30/16.
+//  Copyright © 2016 Victor Frolov. All rights reserved.
+//
+
+import UIKit
+
+class AutoCompleteTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
