c392676e20f8f9ba393856481e24e1a8cf15f2a4
Victor Frolov <vicfrolov@gmail.com>
2017-02-23 01:21:20 -0800

    fixed error on root page with no index, search working for keys, tags, price, not hub yet

--- webapp/src/custom.js
+++ webapp/src/custom.js
@@ -1,4 +1,5 @@
-"use strict" 
+"use strict"
+
 $(function() {
     $('.slider').slider();
     $('ul.tabs').tabs();
@@ -25,15 +26,20 @@ $(function() {
 
 
     $("#search-button-main-page").on('click', () => {
-        keysInput = $("#main-keys").val();
-        hubInput = "todo";
-        tagsInput = "todo";
-        priceMaxInput = $("#main-price").val();
+        let keysInput = $("#main-keys").val().toLowerCase().trim().split(/\s+/);
+        let hubInput = "";
+        let tagsInput = $('#main-tags').textext()[0].tags()._formData;
+        let priceMaxInput = $("#main-price").val().length > 0 ?  $("#main-price").val() : "9999";
+
+        for (let i = 0; i < tagsInput.length; i += 1) {
+            tagsInput[i] = tagsInput[i].toLowerCase();
+        }
         
-        // window.location.href = `/find/find.html#key=\${keysInput}?hub=\${hubInput}?tags=\${tagsInput}?priceMin=1?priceMax=\${priceMaxInput}`;
+        window.location.href = `/find/find.html#key=${keysInput}?hub=${hubInput}?tags=${tagsInput}?priceMin=1?priceMax=${priceMaxInput}`;
     })
 
-    if (window.location.pathname === "/index.html") {
+    if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+        console.log("at index page");
         setTimeout(() => { fadingBlurbs(blurbLeft) }, 1000);
         initializeTagTextExt('#main-tags')
 
