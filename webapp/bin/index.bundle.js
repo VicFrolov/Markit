59b576eb39a2941c09ee15366907cc64baac925f
J.B. Morris <jb0001994@aol.com>
2017-04-21 19:00:07 -0700

    changed FrontPagePhoto format

--- webapp/bin/index.bundle.js
+++ webapp/bin/index.bundle.js
@@ -3516,9 +3516,12 @@
 	        let parentDiv = $(this).parent().parent().parent();
 	        let imageDiv = parentDiv[0].children[2];
 	        let itemID = $(imageDiv)[0].children[0].id;
+	        let temp = $(imageDiv);
+	        console.log(temp);
 	        window.location.href = `/items/item.html#item=${itemID}`;
 	    });
 	});
+
 
 /***/ }
 /******/ ]);
